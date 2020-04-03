01790f40b433410ad77bb4fccbf7acad15618edd
pmpmm-iscteiul <37818617+pmpmm@users.noreply.github.com>
2020-04-03 16:26:58 +0100

    Update importLog.bat

--- grupo20/importLog.bat
+++ grupo20/importLog.bat
@@ -1,19 +1,19 @@
 @echo off
-mysql -u root -p -P 29999 "new_schema2" -e "CALL ExportarLogs();"
+mysql -u root -p -P 29999 "bdorigem" -e "CALL ExportarLogs();"
 if exist logutilizador.csv(
 	if exist logrondaextra.csv(
 		if exist logrondaplaneada.csv(
 			if exist logsistema.csv(
 				if exist logmedicoes.csv(
-					mysql -uroot -p -P 29999 --local-infile "new_schema2" < importaux.sql
+					mysql -uroot -p -P 29999 --local-infile "bddestino" < importaux.sql
 				)
 			)
 		)
 	)
 )	
 else (
-	mysql -u root -p -P 29999 "new_schema2" -e "CALL ExportarLogs();"
-	logutilizador.csv mysql -uroot -p -P 29999 --local-infile "new_schema2" < importaux.sql
+	mysql -u root -p -P 29999 "bdorigem" -e "CALL ExportarLogs();"
+	logutilizador.csv mysql -uroot -p -P 29999 --local-infile "bddestino" < importaux.sql
 	)
 	
 del *.csv
