e433514b44d47dfe935a6c85f3f6d18aedd48c9a
pmpmm-iscteiul <37818617+pmpmm@users.noreply.github.com>
2020-04-03 16:56:03 +0100

    Update importLog.bat

--- grupo20/importLog.bat
+++ grupo20/importLog.bat
@@ -1,19 +1,38 @@
 @echo off
+:start
+goto :export
+
+
+:export
 mysql -u root -p -P 29999 "bdorigem" -e "CALL ExportarLogs();"
-if exist logutilizador.csv(
-	if exist logrondaextra.csv(
-		if exist logrondaplaneada.csv(
-			if exist logsistema.csv(
-				if exist logmedicoes.csv(
-					mysql -uroot -p -P 29999 --local-infile "bddestino" < importaux.sql
-				)
-			)
-		)
-	)
-)	
-else (
-	mysql -u root -p -P 29999 "bdorigem" -e "CALL ExportarLogs();"
-	logutilizador.csv mysql -uroot -p -P 29999 --local-infile "bddestino" < importaux.sql
-	)
-	
+goto :fileassurance
+
+
+
+:fileassurance
+if exist logutilizador.csv (
+	if exist logrondaextra.csv (
+		if exist logrondaplaneada.csv (
+			if exist logsistema.csv (
+				if exist logmedicoes.csv (
+					goto :import
+
+				)else goto :export
+			)else goto :export
+		)else goto :export
+	)else goto :export
+)else goto :export	
+
+
+
+:import
+logutilizador.csv mysql -uroot -p -P 29999 --local-infile "bddestino" < importaux.sql
+goto :delete
+
+
+:delete
 del *.csv
+goto :end
+
+:end
+echo Migration Complete
