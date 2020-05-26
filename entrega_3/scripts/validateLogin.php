02458d72869d38e0f3b989336bb37f4dfb53293d
mdiaz1967 <39262857+mdiaz1967@users.noreply.github.com>
2020-05-26 22:02:09 +0100

    Update validateLogin.php

--- entrega_3/scripts/validateLogin.php
+++ entrega_3/scripts/validateLogin.php
@@ -1,6 +1,6 @@
 	<?php
 	$url="192.168.88.110";
-	$database="main"; // Alterar nome da BD se necessario
+	$database="Main"; // Alterar nome da BD se necessario
     $conn = mysqli_connect($url,$_POST['username'],$_POST['password'],$database);
 	$query_select = "SELECT current_role()";
 	$result_query_select = mysqli_query($conn, $query_select); 
