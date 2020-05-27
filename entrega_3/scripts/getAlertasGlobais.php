8f52b2935ddd5906ed1a688832e2cc21a841b8bf
mdiaz1967 <39262857+mdiaz1967@users.noreply.github.com>
2020-05-28 00:04:26 +0100

    Add files via upload

--- entrega_3/scripts/getAlertasGlobais.php
+++ entrega_3/scripts/getAlertasGlobais.php
@@ -1,27 +1,27 @@
-	<?php
-	$url="192.168.88.110";
-	$database="main"; // Alterar nome da BD se necessario	
-    $conn = mysqli_connect($url,$_POST['username'],$_POST['password'],$database);	
-	// Alterar nome da tabela Alerta e nome do campo DataHoraMedicao se necessario
-	$sql = "SELECT * from Alerta where DATE(Alerta.DataHoraMedicao) = '" . $_POST['date'] . "';";	
-	$result = mysqli_query($conn, $sql);
-	$response["avisos"] = array();
-	if ($result){
-		if (mysqli_num_rows($result)>0){
-			while($r=mysqli_fetch_assoc($result)){
-				$ad = array();
-				// Alterar nome dos campos se necessario
-				$ad["DataHoraMedicao"] = $r['DataHoraMedicao'];
-				$ad["TipoSensor"] = $r['TipoSensor'];
-				$ad["ValorMedicao"] = $r['ValorMedicao'];
-				$ad["Limite"] = $r['Limite'];
-				$ad["Descricao"] = $r['Descricao'];
-				$ad["Controlo"] = $r['Controlo'];
-				$ad["Extra"] = $r['Extra'];
-				array_push($response["avisos"], $ad);
-			}
-		}	
+<?php
+$url = "192.168.88.110";
+$database = "main"; // Alterar nome da BD se necessario	
+$conn = mysqli_connect($url, $_POST['username'], $_POST['password'], $database);
+// Alterar nome da tabela Alerta e nome do campo DataHoraMedicao se necessario
+$sql = "SELECT * from Alerta where DATE(Alerta.DataHoraMedicao) = '" . $_POST['date'] . "';";
+$result = mysqli_query($conn, $sql);
+$response["avisos"] = array();
+if ($result) {
+	if (mysqli_num_rows($result) > 0) {
+		while ($r = mysqli_fetch_assoc($result)) {
+			$ad = array();
+			// Alterar nome dos campos se necessario
+			$ad["DataHoraMedicao"] = $r['DataHoraMedicao'];
+			$ad["TipoSensor"] = $r['TipoSensor'];
+			$ad["ValorMedicao"] = $r['ValorMedicao'];
+			$ad["Limite"] = $r['Limite'];
+			$ad["Descricao"] = $r['Descricao'];
+			$ad["Controlo"] = $r['Controlo'];
+			$ad["Extra"] = $r['Extra'];
+			array_push($response["avisos"], $ad);
+		}
 	}
-	$json = json_encode($response["avisos"]);
-	echo $json;
-	mysqli_close ($conn);
+}
+$json = json_encode($response["avisos"]);
+echo $json;
+mysqli_close($conn);
--- entrega_3/scripts/validateLogin.php
+++ entrega_3/scripts/validateLogin.php
@@ -1,15 +1,15 @@
-	<?php
-	$url="192.168.88.110";
-	$database="main"; // Alterar nome da BD se necessario
-    $conn = mysqli_connect($url,$_POST['username'],$_POST['password'],$database);
-	$query_select = "SELECT current_role()";
-	$result_query_select = mysqli_query($conn, $query_select); 
-	$conn->next_result();
-	$administrador = mysqli_fetch_assoc($result_query_select)['current_role()'];
-	if($administrador == "Administradores" or $administrador == "Directores" or $administrador == "Chefes") { // Alterar role se necessario
-		$response["valid"] = array();
-		$json = json_encode($response["valid"]);
-		echo $json;
-	}
-	$result_query_select->close();
-	mysqli_close ($conn);
+<?php
+$url = "192.168.88.110";
+$database = "main"; // Alterar nome da BD se necessario
+$conn = mysqli_connect($url, $_POST['username'], $_POST['password'], $database);
+$query_select = "SELECT current_role()";
+$result_query_select = mysqli_query($conn, $query_select);
+$conn->next_result();
+$administrador = mysqli_fetch_assoc($result_query_select)['current_role()'];
+if ($administrador == "Administradores" or $administrador == "Directores" or $administrador == "Chefes") { // Alterar role se necessario
+	$response["valid"] = array();
+	$json = json_encode($response["valid"]);
+	echo $json;
+}
+$result_query_select->close();
+mysqli_close($conn);
